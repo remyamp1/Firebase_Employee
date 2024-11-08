1e62e68a3385c18fd95ff854162127233766a30c
aleenakrish <aleenakrish59@gmail.com>
2024-11-08 13:30:32 +0530

    8-11-24

--- lib/database.dart
+++ lib/database.dart
@@ -1,7 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
 class Database {
-  static Future addEmpolyeeDetails(Map<String,dynamic>employeeInfoMap,String id)async{
-    return await FirebaseFirestore.instance.collection("Employee").doc(id).set(employeeInfoMap);
+  static Future addEmpolyeeDetails(
+      Map<String, dynamic> employeeInfoMap, String id) async {
+    return await FirebaseFirestore.instance
+        .collection("Employee")
+        .doc(id)
+        .set(employeeInfoMap);
   }
+
+  static Future<Stream<QuerySnapshot>> getEmployeeDetails() async {
+    return await FirebaseFirestore.instance.collection("Employee").snapshots();
+  }
+  static Future deleteEmployeeDetails(String id) async{
+  return await FirebaseFirestore.instance.collection("Employee").doc(id).delete();
+}
 }
--- lib/first.dart
+++ lib/first.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_empolye/database.dart';
 import 'package:firebase_empolye/second.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +11,84 @@ class Firstscreen extends StatefulWidget {
 }
 
 class _FirstscreenState extends State<Firstscreen> {
+  TextEditingController Namecontroller = TextEditingController();
+  TextEditingController Agecontroller = TextEditingController();
+  TextEditingController locationcontroller = TextEditingController();
+  Stream<QuerySnapshot>? EmployeeStrem;
+
+  getontheload() async {
+    EmployeeStrem = await Database.getEmployeeDetails();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getontheload();
+  }
+
+  Widget allEmployeeDetails() {
+    return StreamBuilder<QuerySnapshot>(
+        stream: EmployeeStrem,
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshot.hasError) {
+            return Center(
+              child: Text("Error:${snapshot.error}"),
+            );
+          }
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return Center(child: Text("No employee data available."));
+          }
+          return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                DocumentSnapshot ds = snapshot.data!.docs[index];
+                return Container(
+                  margin: EdgeInsets.only(bottom: 20),
+                  color: Colors.lightBlue,
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            "Name:" + (ds["Name"] ?? 'N/A'),
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          Spacer(),
+                          GestureDetector(
+                            onTap: () {},
+                            child: GestureDetector(
+                                onTap: () async {
+                                  await Database.deleteEmployeeDetails(
+                                      ds['Id']);
+                                },
+                                child: Icon(
+                                  Icons.delete,
+                                  color: Colors.red,
+                                )),
+                          )
+                        ],
+                      ),
+                      Text(
+                        "Age:" + (ds["Age"] ?? 'N/A').toString(),
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text(
+                        "Location:" + (ds["Location"] ?? 'N/A'),
+                        style: TextStyle(fontSize: 20),
+                      )
+                    ],
+                  ),
+                );
+              });
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,14 +97,19 @@ class _FirstscreenState extends State<Firstscreen> {
           child: Text("Flutter Firebase"),
         ),
       ),
-      body: Center(
-        child: FloatingActionButton(
-          onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => Secondscreen()));
-          },
-          child: Icon(Icons.add),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(child: allEmployeeDetails()),
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => Secondscreen()));
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
